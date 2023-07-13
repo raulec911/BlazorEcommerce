d823fd41e494964ec0cb3d5b5420be264025578c
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-07-13 13:57:18 -0500

    Adding & Using Best Practices

---
+++ BlazorEcommerce/Server/Services/ProductService/ProductService.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazorEcommerce.Server.Services.ProductService
+{
+
+    public class ProductService : IProductService
+    {
+        private readonly DataContext _context;
+        public ProductService(DataContext context)
+        {
+            _context = context;
+        }
+        public Task<ServiceResponse<Product>> GetProductAsync(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<List<Product>>> GetProductsAsync()
+        {
+            var response = new ServiceResponse<List<Product>>
+            {
+                Data = await _context.Products.ToListAsync()
+            };
+
+            return response;
+        }
+
+        public Task<ServiceResponse<List<Product>>> GetProductsByCategory(string categoryUrl)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
