1b9c0cdbce50e85def9e6c1cdabc211dbadf4c99
Raúl Sánchez Campos <raulec911@hotmail.com>
2023-07-15 15:26:16 -0500

    Pagination

--- BlazorEcommerce/Client/Services/ProductService/ProductService.cs
+++ BlazorEcommerce/Client/Services/ProductService/ProductService.cs
@@ -10,6 +10,9 @@
 
         public List<Product> Products { get; set; } = new List<Product>();
         public string Message { get; set; } = "Loading products...";
+        public int CurrentPage { get; set; } = 1;
+        public int PageCount { get; set; } = 0;
+        public string LastSearchText { get; set; } = string.Empty;
 
         public event Action ProductsChanged;
         public async Task<ServiceResponse<Product>> GetProduct(int productId)
@@ -21,11 +24,17 @@
         public async Task GetProducts(string categoryUrl = null)
         {
             var result = categoryUrl == null ?
-               await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product") :
+               await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>("api/product/featured") :
                await _http.GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/category/{categoryUrl}");
 
             if (result != null && result.Data != null)
                 Products = result.Data;
+
+            CurrentPage = 1;
+            PageCount = 0;
+
+            if (Products.Count == 0)
+                Message = "No products found";
 
             ProductsChanged.Invoke();
         }
@@ -37,16 +46,19 @@
             return result.Data;
         }
 
-        public async Task SearchProducts(string searchText)
+        public async Task SearchProducts(string searchText, int page = 1)
         {
+            LastSearchText = searchText;
             var result = await _http
-                .GetFromJsonAsync<ServiceResponse<List<Product>>>($"api/product/search/{searchText}");              
-
+                 .GetFromJsonAsync<ServiceResponse<ProductSearchResult>>($"api/product/search/{searchText}/{page}");
             if (result != null && result.Data != null)
-                Products = result.Data;
-
+            {
+                Products = result.Data.Products;
+                CurrentPage = result.Data.CurrentPage;
+                PageCount = result.Data.Pages;
+            }
             if (Products.Count == 0) Message = "No products found.";
-                ProductsChanged?.Invoke();
+            ProductsChanged?.Invoke();
         }
     }
 }
